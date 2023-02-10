f14c06bc089fec8adbb8e1dd6bf48980806d119a
sushma3232 <121096745+sushma3232@users.noreply.github.com>
2023-02-10 10:25:08 +0530

    Add files via upload

---
+++ fun1.py
@@ -0,0 +1,15 @@
+ Write a function named ask_question which will print the text "Once".
+# def ask_question():
+#     print("who is the founder of facebook")
+# ask_question()
+# ask_question()
+# ask_question()
+# ask_question()
+# ask_question()
+
+def ask_question():
+    i=1
+    while i<=100:
+        print("who is the founder of facebook")
+        i=i+1
+ask_question()
